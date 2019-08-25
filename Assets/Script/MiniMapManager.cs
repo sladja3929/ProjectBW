af50f890af57429bd65f5df3e2b638b96ff29c7c
WonJun Na <43125863+sladja3929@users.noreply.github.com>
2019-08-26 06:17:43 +0900

    맨션추가, 미니맵수정
    moveCamera ->MoveCamera로 수정

--- Assets/Script/MiniMapManager.cs
+++ Assets/Script/MiniMapManager.cs
@@ -15,6 +15,15 @@ public class MiniMapManager : MonoBehaviour
     private Vector3 miniMap_Position_Slum_Street1 = new Vector3(-10.4f, -4.5f, -1f);
     private Vector3 miniMap_Position_Slum_Street2 = new Vector3(-10f, -6.5f, -1f);
     private Vector3 miniMap_Position_Market_Street1 = new Vector3(-10.5f, 2f, -1f);
+    private Vector3 miniMap_Position_Market_Street2 = new Vector3(-10.5f, 2f, -1f);
+    private Vector3 miniMap_Position_Market_Street3 = new Vector3(-10.5f, 2f, -1f);
+    private Vector3 miniMap_Position_Village_Street1 = new Vector3(2.5f, 13.6f, -1f);
+    private Vector3 miniMap_Position_Village_Street2 = new Vector3(2.5f, 11.4f, -1f);
+    private Vector3 miniMap_Position_Village_Street3 = new Vector3(2.5f, 8.8f, -1f);
+    private Vector3 miniMap_Position_Mansion_Street1 = new Vector3(10f, 3.6f, -1f);
+    private Vector3 miniMap_Position_Mansion_Street2 = new Vector3(10.5f, 1f, -1f);
+    private Vector3 miniMap_Position_Mansion_Street3 = new Vector3(10f, -1f, -1f);
+    private Vector3 miniMap_Position_Downtown_Street1 = new Vector3(-0.2f, -0.5f, -1f);
 
     void Start()
     {
@@ -50,6 +59,42 @@ public class MiniMapManager : MonoBehaviour
 
         else if (PlayerManager.instance.GetCurrentPosition() == "Market_Street1") {
             arrow.transform.localPosition = miniMap_Position_Market_Street1;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Market_Street2") {
+            arrow.transform.localPosition = miniMap_Position_Market_Street2;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Market_Street3") {
+            arrow.transform.localPosition = miniMap_Position_Market_Street3;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Village_Street1") {
+            arrow.transform.localPosition = miniMap_Position_Village_Street1;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Village_Street2") {
+            arrow.transform.localPosition = miniMap_Position_Village_Street2;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Village_Street3") {
+            arrow.transform.localPosition = miniMap_Position_Village_Street3;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Mansion_Street1") {
+            arrow.transform.localPosition = miniMap_Position_Mansion_Street1;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Mansion_Street2") {
+            arrow.transform.localPosition = miniMap_Position_Mansion_Street2;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Mansion_Street3") {
+            arrow.transform.localPosition = miniMap_Position_Mansion_Street3;
+        }
+
+        else if (PlayerManager.instance.GetCurrentPosition() == "Downtown_Street1") {
+            arrow.transform.localPosition = miniMap_Position_Downtown_Street1;
         }
     }
 }
